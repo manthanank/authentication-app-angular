21253b61f32623004d4c50c7dc60882ebdc26fcf
Manthan Ankolekar <manthan.ank@gmail.com>
2023-01-09 18:36:26 +0530

    changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ForgotPasswordComponent,
+    SignInComponent,
+    SignUpComponent,
+    VerifyEmailComponent,
+    ForgotPasswordComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-    production: true
+    production: true,
+    firebase: {
+        projectId: 'authentication-app-angular',
+        appId: '1:555460909003:web:509f1b75a9ad3e7eb7884e',
+        storageBucket: 'authentication-app-angular.appspot.com',
+        apiKey: 'AIzaSyB12G3PbrUQgsoJ8VlwkWAs3l08MsCgH2s',
+        authDomain: 'authentication-app-angular.firebaseapp.com',
+        messagingSenderId: '555460909003',
+        measurementId: 'G-6J7XSSTGPX',
+    },
 }
