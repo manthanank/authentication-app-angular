59a649261f64e877ec5e7b918693b109a99f8637
Manthan Ankolekar <manthan.ank@gmail.com>
2023-01-09 16:56:54 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,11 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 @NgModule({
   declarations: [
     AppComponent
@@ -26,10 +31,15 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     SignUpComponent,
     ForgotPasswordComponent,
     VerifyEmailComponent,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // provideAuth(() => getAuth()),
+    // provideDatabase(() => getDatabase()),
+    // provideFirestore(() => getFirestore()),
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,14 @@ export const environment = {
     messagingSenderId: '136711169403',
     measurementId: 'G-G4WSF9Y6HG',
   },
+  // firebase: {
+  //   projectId: 'angular-firebase-auth-ap-f8869',
+  //   appId: '1:136711169403:web:5314d74d6751458fccc9de',
+  //   storageBucket: 'angular-firebase-auth-ap-f8869.appspot.com',
+  //   apiKey: 'AIzaSyBG8DZup7HXhDZPrjyCBeFGv41wVwGKHus',
+  //   authDomain: 'angular-firebase-auth-ap-f8869.firebaseapp.com',
+  //   messagingSenderId: '136711169403',
+  //   measurementId: 'G-G4WSF9Y6HG',
+  // },
   production: false
 };
